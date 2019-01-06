1fddc1741e9fed5664607b1133a932b034c90772
Chase Thomas <cj.thomas19@students.bchigh.edu>
2019-01-06 12:11:53 -0500

    Prototype Vector Class

--- src/math/Vector.java
+++ src/math/Vector.java
@@ -1,24 +1,95 @@
 package math;
 
 public class Vector {
-	//Initializes an array to hold the vector's values
-	public int[] values;
-	
-	
-	//Default constructor uses 2 numbers for 2 dimensions
-	public Vector() {
-		values = new int[2];
+	private double[] coords;
+	public Vector(double[] a) {
+		
+		coords = a;
 	}
 	
-	//Constructor for ease of use with 2 dimensional math
-	public Vector(int x, int y) {
-		values = new int[2];
-		values[0] = x;
-		values[1] = y;
+	public Vector(double x, double y) {
+		coords = new double[2];
+		coords[0] = x;
+		coords[1] = y;
 	}
 	
-	//Constructor for any sized vector
-	public Vector(int num) {
-		values = new int[num];
+	public Vector(double x, double y, double z) {
+		coords = new double[3];
+		coords[0] = x;
+		coords[1] = y;
+		coords[2] = z;
+	}
+	
+	public Vector plus(Vector that) {
+		double[] result = new double[that.coords.length>coords.length?that.coords.length:coords.length];
+		for(int i = 0; i < result.length; i++) {
+			if(i >= that.coords.length) result[i] = coords[i];
+			else if(i >= this.coords.length) result[i] = that.coords[i];
+			else result[i] = that.coords[i] + coords[i];
+		}
+		
+		return new Vector(result);
+	}
+	
+	public Vector minus(Vector that) {
+		double[] result = new double[that.coords.length>coords.length?that.coords.length:coords.length];
+		for(int i = 0; i < result.length; i++) {
+			if(i >= that.coords.length) result[i] = coords[i];
+			else if(i >= this.coords.length) result[i] = -that.coords[i];
+			else result[i] = coords[i] - that.coords[i];
+		}
+		
+		return new Vector(result);
+	}
+	
+	public Vector scale(double alpha) {
+		double[] result = new double[coords.length];
+		for(int i = 0; i < result.length; i++) result[i] = coords[i] * alpha;
+		return new Vector(result);
+	}
+	
+	public double dot(Vector that) {
+		int len = that.coords.length>coords.length?that.coords.length:coords.length;
+		double result = 0;
+		for(int i = 0; i < len; i++) {
+			if(i < that.coords.length && i < this.coords.length) result += coords[i] * that.coords[i];
+		}
+		
+		return result;
+	}
+	
+	public double magnitude() {
+		double sum = 0;
+		for(int i = 0; i < coords.length; i++) {
+			sum += Math.pow(coords[i], 2);
+		}
+		return Math.sqrt(sum);
+	}
+	
+	public Vector direction() {
+		double[] result = new double[coords.length];
+		for(int i = 0; i < result.length; i++) {
+			result[i] = coords[i] / magnitude();
+		}
+		
+		return new Vector(result);
+	}
+	
+	public double cartesian(int i) {
+		return coords[i]; 
+	}
+	
+	public String toString() {
+		String str = "(";
+		for(int i = 0; i < coords.length-1; i++) {
+			str = str + coords[i] + ",";
+		}
+		
+		str = str + coords[coords.length-1] + ")";
+		return str;
+	}
+	
+	public Vector rotate(double theta) {
+		return new Vector(magnitude() * Math.cos(theta), magnitude() * Math.sin(theta));
 	}
 }
